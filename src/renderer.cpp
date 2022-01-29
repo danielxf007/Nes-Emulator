3674f5c0d1696171aa8df6acebc5b30000f0019d
danielxf007 <daniel.higuita007@gmail.com>
2022-01-29 18:52:52 -0500

    added gui

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,6 +1,21 @@
 #include "renderer.h"
 
-Renderer::Renderer(){
+
+Renderer::Renderer(EmuGui *emu_gui){
+    this->emu_gui = emu_gui;
+}
+
+Renderer::~Renderer(){
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    window = nullptr;
+    SDL_Quit();
+}
+
+void Renderer::init(){
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
         printf("Error: %s\n", SDL_GetError());
     }else{
@@ -18,30 +33,19 @@ Renderer::Renderer(){
         gl_context = SDL_GL_CreateContext(window);
         SDL_GL_MakeCurrent(window, gl_context);
         SDL_GL_SetSwapInterval(1); // Enable vsync
-        // Setup Dear ImGui context
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-        // Setup Dear ImGui style
-        ImGui::StyleColorsDark();
-        //ImGui::StyleColorsClassic();
-        // Setup Platform/Renderer backends
-        ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-        ImGui_ImplOpenGL3_Init(glsl_version);
-        io  = ImGui::GetIO(); (void)io;
     }
-}
-
-Renderer::~Renderer(){
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-    SDL_GL_DeleteContext(gl_context);
-    gl_context = NULL;
-    SDL_DestroyWindow(window);
-    window_surface = NULL;
-    SDL_Quit();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    io  = ImGui::GetIO(); (void)io;
+    emu_gui->init();
 }
 
 void Renderer::render(){
@@ -49,66 +53,7 @@ void Renderer::render(){
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
-        bool open = false, save = false;
-        if(ImGui::BeginMainMenuBar())
-        {
-            if (ImGui::BeginMenu("Menu"))
-            {
-                if (ImGui::MenuItem("Open", NULL))
-                    open = true;
-            if (ImGui::MenuItem("Save", NULL))
-                    save = true;
-                
-            ImGui::EndMenu();
-            }
-            ImGui::EndMainMenuBar();
-        }
-        
-        //Remember the name to ImGui::OpenPopup() and showFileDialog() must be same...
-        if(open)
-            ImGui::OpenPopup("Open File");
-        if(save)
-            ImGui::OpenPopup("Save File");
-            
-        /* Optional third parameter. Support opening only compressed rar/zip files. 
-        * Opening any other file will show error, return false and won't close the dialog.
-        */
-        if(file_dialog.showFileDialog("Open File", imgui_addons::ImGuiFileBrowser::DialogMode::OPEN, ImVec2(700, 310), ".rar,.zip,.7z"))
-        {
-            std::cout << file_dialog.selected_fn << std::endl;      // The name of the selected file or directory in case of Select Directory dialog mode
-            std::cout << file_dialog.selected_path << std::endl;    // The absolute path to the selected file
-        }
-        if(file_dialog.showFileDialog("Save File", imgui_addons::ImGuiFileBrowser::DialogMode::SAVE, ImVec2(700, 310), ".png,.jpg,.bmp"))
-        {
-            std::cout << file_dialog.selected_fn << std::endl;      // The name of the selected file or directory in case of Select Directory dialog mode
-            std::cout << file_dialog.selected_path << std::endl;    // The absolute path to the selected file
-            std::cout << file_dialog.ext << std::endl;              // Access ext separately (For SAVE mode)
-            //Do writing of files based on extension here
-        }
-        ImGui::Begin("Controls");
-            ImGui::Button("one", ImVec2(50, 50));
-        ImGui::End();
-
-        ImGui::Begin("Tables");
-        
-       if (ImGui::BeginTable("table_padding", 2, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg))
-        {
-            ImGui::TableSetupColumn("Register");
-            ImGui::TableSetupColumn("Value");
-            ImGui::TableHeadersRow();
-
-            for (int row = 0; row < 2; row++)
-            {
-                ImGui::TableNextRow();
-                for (int column = 0; column < 2; column++)
-                {
-                    ImGui::TableSetColumnIndex(column);
-                    ImGui::Text("Hello %d,%d", column, row);
-                }
-            }
-            ImGui::EndTable();
-        }
-        ImGui::End();
+        emu_gui->render();
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
