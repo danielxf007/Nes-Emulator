e5da09ff08429e0b4b3a1202077680b7aa66a13f
danielxf007 <daniel.higuita007@gmail.com>
2022-01-29 18:53:23 -0500

    added utils

--- src/renderer.h
+++ src/renderer.h
@@ -5,23 +5,24 @@
 #include "imgui_impl_opengl3.h"
 #include "ImGuiFileBrowser.h"
 #include "imgui_internal.h"
+#include "gui.h"
+#include "util.h"
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
-#include <iostream>
 
 class Renderer{
     private:
+        const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         const char* glsl_version = "#version 130";
         SDL_Window *window;
         SDL_GLContext gl_context;
-        SDL_Surface *window_surface;
-        const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-        imgui_addons::ImGuiFileBrowser file_dialog;
+        EmuGui *emu_gui;
         ImGuiIO io;
     public:
-        Renderer();
+        Renderer(EmuGui *emu_gui);
         ~Renderer();
+        void init();
         void render();
 };
 
