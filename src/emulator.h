470f8d54edd4cb3cba535c933f07bfabfe08198a
danielxf007 <daniel.higuita007@gmail.com>
2022-01-29 18:52:18 -0500

    added gui

--- src/emulator.h
+++ src/emulator.h
@@ -1,8 +1,11 @@
-#include "states.h"
+#include "states_flags.h"
 #include "renderer.h"
+#include "gui.h"
+#include <iostream>
 
 class Emulator{
     private:
+        EmuGui *emu_gui;
         Renderer *renderer;
         int curr_state;
     public:
