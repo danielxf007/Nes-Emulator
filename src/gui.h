d1ba36d7c216e70935baca855d35141e2972191e
danielxf007 <daniel.higuita007@gmail.com>
2022-01-29 21:10:25 -0500

    declared memory editor

--- src/gui.h
+++ src/gui.h
@@ -6,6 +6,7 @@
 #include "imgui_impl_opengl3.h"
 #include "ImGuiFileBrowser.h"
 #include "imgui_internal.h"
+#include "../UI/MemoryEditor/imgui_memory_editor.h"
 #include "util.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
@@ -15,12 +16,15 @@ class EmuGui{
     private:
         int *emu_state;
         imgui_addons::ImGuiFileBrowser *file_dialog;
+        MemoryEditor *mem_edit;
         int file_dialog_flags;
+        int data[64];
     public:
         EmuGui(int *emu_state);
         ~EmuGui();
         void init();
         void renderFileDialog();
+        void renderMemEditor();
         void render();
         std::string getRomPath();
 };
