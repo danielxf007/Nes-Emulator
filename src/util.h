814fa2d4509b2506ca0590fc60a043aed4d5763d
danielxf007 <daniel.higuita007@gmail.com>
2022-01-30 22:33:09 -0500

    Added getNibble utility

--- src/util.h
+++ src/util.h
@@ -6,6 +6,10 @@ template<class T> inline bit getBit(T number, u8 bit_pos){
     return ((number & (static_cast<T>(0x1) << bit_pos)) >> bit_pos);
 }
 
+template<class T> inline bit getNibble(T number, u8 nibble_pos){
+    return ((number & (static_cast<T>(0xF) << (4*nibble_pos))) >> (4*nibble_pos));
+}
+
 template<class T> inline void setBit(T &number, bit value, u8 bit_pos){
     number = (number & ~(value << bit_pos)) | (value << bit_pos);
 }
