1cd504afc67168499fe3b0d181a7ce18aab6598e
danielxf007 <daniel.higuita007@gmail.com>
2022-01-30 22:36:21 -0500

    prototyped memory mapped no bank switch

---
+++ src/memory_map.h
@@ -0,0 +1,37 @@
+#ifndef MEMORY_MAP_H
+#define MEMORY_MAP_H
+#include "data_types.h"
+#include "states_flags.h"
+#include "cartridge.h"
+
+struct Peripherals{
+    byte zero_page[0x100], stack[0x100], ram[0x600], io_regs0[8], io_regs1[0x20];
+    byte expansion_rom[0x1FE0], sram[0x2000];
+    Cartridge *cartridge;
+};
+
+class NRom256{
+
+    private:
+        Peripherals *peripherals;
+    
+    public:
+        NRom256(Peripherals *peripherals);
+        ~NRom256();
+        byte read(word addr);
+        void write(word addr, byte value);
+};
+
+class MemoryMapper{
+    private:
+        NRom256 *n_rom_256;
+    public:
+        Peripherals *peripherals;
+        int mapping_type;
+    public:
+        MemoryMapper();
+        ~MemoryMapper();
+        byte read(word addr);
+        void write(word addr, byte value);    
+};
+#endif
