9db9ac4d6c8c46ce41625fb11d959531316c45eb
danielxf007 <daniel.higuita007@gmail.com>
2022-02-03 12:12:04 -0500

    added getNBits utility

--- src/util.h
+++ src/util.h
@@ -6,6 +6,10 @@ template<class T> inline bit getBit(T number, u8 bit_pos){
     return ((number & (static_cast<T>(0x1) << bit_pos)) >> bit_pos);
 }
 
+template<class T> inline bit getNBits(T number, u8 n_bits, u8 pos){
+    return ((number & ((~(~(static_cast<T>(0)) << n_bits)) << pos)) >> pos);
+}
+
 template<class T> inline bit getNibble(T number, u8 nibble_pos){
     return ((number & (static_cast<T>(0xF) << (4*nibble_pos))) >> (4*nibble_pos));
 }
