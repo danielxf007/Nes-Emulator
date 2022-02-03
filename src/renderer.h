810073f220253963cadacec7346f905714aad784
danielxf007 <daniel.higuita007@gmail.com>
2022-02-03 12:23:42 -0500

    added shader class

--- src/renderer.h
+++ src/renderer.h
@@ -1,5 +1,8 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <SDL2/SDL_opengl.h>
 #include "imgui.h"
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
@@ -7,14 +10,16 @@
 #include "imgui_internal.h"
 #include "gui.h"
 #include "util.h"
+#include "shader.h"
 #include <stdio.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+
 
 class Renderer{
     private:
+        Shader *shader;
         const ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         const char* glsl_version = "#version 130";
+        unsigned int VBO, VAO;
         SDL_Window *window;
         SDL_GLContext gl_context;
         EmuGui *emu_gui;
