bd98ba747a6a7d697fa7cfcfb89f38e192e9c352
danielxf007 <daniel.higuita007@gmail.com>
2022-02-03 12:18:26 -0500

    added unsined 32 bit type

--- src/data_types.h
+++ src/data_types.h
@@ -4,12 +4,10 @@
 
 typedef uint8_t u8;
 typedef uint16_t u16;
+typedef uint32_t u32;
 typedef uint8_t bit;
 typedef uint8_t nibble;
 typedef uint8_t byte;
 typedef uint16_t word;
-typedef struct{
-    byte A, X, Y, P, SP;
-    word PC;
-}CPUContext;
+
 #endif
